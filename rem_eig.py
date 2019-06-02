4b4e0d2715123b3d52280bd23bbbc44a9e3611a9
Sneha-Perugupalli <51277126+Sneha-Perugupalli@users.noreply.github.com>
2019-06-02 01:51:22 -0400

    removes nodes based on eigenvector centrality

---
+++ rem_eig.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import sys
+import networkx as nx
+import csv
+import copy
+import random
+import numpy as np
+from random import choice
+from itertools import islice
+import matplotlib.pyplot as plt
+from create_graph import cr_grph
+from eigen import Ei_cc_Dict
+G=nx.Graph()
+(G,edge_data)=cr_grph()
+H=copy.deepcopy(G)
+J1=copy.deepcopy(G)
+J2=copy.deepcopy(G)
+""" 1 simple shortest path"""
+def effected_shortest_paths(G, source, target, k, weight=None):
+       return list(islice(nx.shortest_simple_paths(G, source, target, weight=weight), k))
+routeDict = {}
+to_rem_eig = list(Ei_cc_Dict.keys())
+#rem_edges = ord_bet
+print("Number of nodes", len(list(J1.nodes())))
+
+
+      
+for i in range(1457):
+      rem = to_rem_eig[i]
+      #print("To rem", to_rem[i])
+      J2.remove_node(to_rem_eig[i])
+
+print("After Removal", len(list(J2.nodes())))
+
+s = np.random.choice(J2.nodes())
+t = np.random.choice(J2.nodes()) 
+print("Source: ", s)
+print("Target: ", t)
+
+sp_list = []
+sp_list_rem = []
+
+if nx.has_path(J1, s, t):
+      for path in effected_shortest_paths(J1, s, t, 1):
+            total_length = 1
+            for i in range(len(path)-1):
+                  n = path[i]
+                  J1.node[n]['color']='blue'
+                  J1.add_path(n,color= 'blue' )
+                  total_length += 1
+            print("Shortest path before removal")
+            print('{}: {}'.format(path, total_length))
+      nx.write_graphml(J1,"SP_Eig_Bef_removal.graphml")
+else:
+      print("No Path between these nodes, Kindly run again!!")
+if nx.has_path(J2, s, t):      
+      for path in effected_shortest_paths(J2, s, t, 1):
+            total_length = 1
+            for i in range(len(path)-1):
+                  n = path[i]
+                  J2.node[n]['color']='red'
+                  J1.add_path(n,color= 'red' )
+                  total_length += 1
+            print("Shortest path After removal")
+            print('{}: {}'.format(path, total_length))
+      nx.write_graphml(J2,"SP_Eig_Aft_removal.graphml")
+else:
+      print("No Path between these nodes, Kindly run again!!")
+
+print("After Removal - 2 ", len(list(J2.nodes())))
